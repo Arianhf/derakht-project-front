d1e120061738c8f59528402ea954bff3edf42411
alikalatearabi <ali.kalate89@hotmail.com>
2025-02-18 01:59:09 +0330

    hero section of the shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import styles from "./shop.module.scss";
 import Image from "next/image";
 import logo from "@/assets/images/logo2.png";
-import { FaShoppingBasket } from "react-icons/fa";
+import heroImage from "@/assets/images/header.jpg";
 import { Navbar } from "@/components/shared/Navbar";
 
 const ShopPage = () => {
@@ -13,7 +13,14 @@ const ShopPage = () => {
 
   return (
     <div className={styles.shopContainer}>
-      <Navbar logo={logo} />
+      <Navbar logo={logo} basketCount={cartCount} />
+      
+      <div className={styles.heroSection}>
+        <Image src={heroImage} alt="Shop Hero" layout="fill" objectFit="cover" className={styles.heroImage} />
+        <div className={styles.heroOverlay}>
+          <h1 className={styles.heroText}>فروشگاه درخت</h1>
+        </div>
+      </div>
     </div>
   );
 };
