b8fb1423b78d9d8709594b035497dc2caed799ef
GC-Kalateh <ali.kalate89@hotmail.com>
2025-03-12 16:29:08 +0330

    some changes in product card

--- src/components/shop/ProductCard.tsx
+++ src/components/shop/ProductCard.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
-import styles from './ProductCard.module.scss';
-import { toPersianNumber } from '@/utils/convertToPersianNumber';
 import { FaPlus } from 'react-icons/fa';
+import { StaticImageData } from 'next/image';
+import styles from './ProductCard.module.scss';
 
 interface ProductCardProps {
-  imageSrc: string;
+  imageSrc: string | StaticImageData; // Updated type to accept string or StaticImageData
   title: string;
   price: number;
   description: string;
@@ -20,10 +22,10 @@ const ProductCard: React.FC<ProductCardProps> = ({ imageSrc, title, price, descr
       </div>
       <div className={styles.cardBody}>
         <h3 className={styles.cardTitle}>{title}</h3>
-        <p className={styles.cardPrice}>{toPersianNumber(price.toLocaleString())} تومان</p>
+        <p className={styles.cardPrice}>{price.toLocaleString()} تومان</p>
         <p className={styles.cardDescription}>{description}</p>
         <button className={styles.addToCartButton} onClick={onAddToCart}>
-          افزودن به سبد خرید 
+          افزودن به سبد خرید <FaPlus className={styles.plusIcon} />
         </button>
       </div>
     </div>
