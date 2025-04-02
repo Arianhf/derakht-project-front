010da86decc56037e9c498de45ca9638009e37dc
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2025-04-02 17:35:16 +0330

    add shop hero jpg and fix login no suspense.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import React from 'react';
+import React, {Suspense} from 'react';
 import LoginPage from '@/components/login/LoginPage';
 import { Toaster } from 'react-hot-toast';
+import {Loading} from "@/components/shared/Loading";
 
 export default function Login() {
     return (
-        <>
+        <Suspense fallback={<Loading />}>
             <Toaster position="top-center" />
             <LoginPage />
-        </>
+        </Suspense>
     );
 }
